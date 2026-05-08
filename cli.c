8e72656c96cd67fe021a66f78d8391f1993f31d9
Anlige <sometiny.github@gmail.com>
2026-05-08 14:30:42 +0800

    Update cli.c

--- cli.c
+++ cli.c
@@ -18,7 +18,7 @@ void usage(char *prog)
 {
     fprintf(stderr,
             "error: Usage: %s [-q] [-f <4|6>] [-t timeout_sec] [-u "
-            "timeout_usec] <host> <port>\n",
+            "timeout_usec] [-n count] <host> <port>\n",
             prog);
     exit(-1);
 }
@@ -30,6 +30,7 @@ int main(int argc, char *argv[])
     long timeout_sec = 0, timeout_usec = 0;
     struct timeval timeout;
     int verbosity = 1;
+    int count = 1;  // 默认发送1次
 
     if (argc < 3) {
         usage(argv[0]);
@@ -37,7 +38,7 @@ int main(int argc, char *argv[])
 
     char *cptr;
     int c;
-    while ((c = getopt(argc, argv, "qf:t:u:")) != -1) {
+    while ((c = getopt(argc, argv, "qf:t:u:n:")) != -1) {
         switch (c) {
         case 'q':
             verbosity = 0;
@@ -61,6 +62,12 @@ int main(int argc, char *argv[])
             if (cptr == optarg)
                 usage(argv[0]);
             break;
+        case 'n':
+            cptr = NULL;
+            count = strtol(optarg, &cptr, 10);
+            if (cptr == optarg || count <= 0)
+                usage(argv[0]);
+            break;
         default:
             usage(argv[0]);
             break;
@@ -82,32 +89,68 @@ int main(int argc, char *argv[])
         retval = 255;
         goto quit;
     }
-    int sockfd = tcping_socket(host);
-    int result = tcping_connect(sockfd, host, &timeout);
-    tcping_close(sockfd);
-    switch (result) {
-    case TCPING_ERROR:
-        log(verbosity, stderr, "error: %s port %s: %s\n", host->name,
-            host->serv, strerror(errno));
-        retval = 255;
-        break;
-    case TCPING_OPEN:
-        log(verbosity, stdout, "%s port %s open.\n", host->name, host->serv);
-        break;
-    case TCPING_CLOSED:
-        log(verbosity, stdout, "%s port %s closed.\n", host->name, host->serv);
-        retval = 1;
-        break;
-    case TCPING_TIMEOUT:
-        log(verbosity, stdout, "%s port %s user timeout.\n", host->name,
-            host->serv);
-        retval = 2;
-        break;
-    default:
-        log(verbosity, stderr, "error: invalid return value\n");
-        retval = 255;
-        break;
+
+    // 统计变量
+    int success_count = 0;
+    int fail_count = 0;
+    double total_time = 0.0;
+    double min_time = -1.0, max_time = -1.0;
+
+    for (int i = 0; i < count; i++) {
+        int sockfd = tcping_socket(host);
+        double time_ms = 0.0;
+        int result = tcping_connect(sockfd, host, &timeout, &time_ms);
+        tcping_close(sockfd);
+
+        switch (result) {
+        case TCPING_ERROR:
+            log(verbosity, stderr, "error: %s port %s: %s (%.3f ms)\n", host->name,
+                host->serv, strerror(errno), time_ms);
+            fail_count++;
+            retval = 255;
+            break;
+        case TCPING_OPEN:
+            log(verbosity, stdout, "%s port %s open. time=%.3f ms\n", host->name, host->serv, time_ms);
+            success_count++;
+            total_time += time_ms;
+            if (min_time < 0 || time_ms < min_time) min_time = time_ms;
+            if (max_time < 0 || time_ms > max_time) max_time = time_ms;
+            break;
+        case TCPING_CLOSED:
+            log(verbosity, stdout, "%s port %s closed. (%.3f ms)\n", host->name, host->serv, time_ms);
+            fail_count++;
+            retval = 1;
+            break;
+        case TCPING_TIMEOUT:
+            log(verbosity, stdout, "%s port %s user timeout. (%.3f ms)\n", host->name,
+                host->serv, time_ms);
+            fail_count++;
+            retval = 2;
+            break;
+        default:
+            log(verbosity, stderr, "error: invalid return value\n");
+            retval = 255;
+            break;
+        }
+
+        // 如果不是最后一次，稍微延迟一下
+        if (i < count - 1) {
+            usleep(100000);  // 100ms 间隔
+        }
     }
+
+    // 输出统计信息
+    if (verbosity && count > 1) {
+        double success_rate = (double)success_count / count * 100.0;
+        double avg_time = success_count > 0 ? total_time / success_count : 0.0;
+        printf("\n--- %s tcping statistics ---\n", host->name);
+        printf("%d requests: %d success, %d failed, %.1f%% success rate\n",
+               count, success_count, fail_count, success_rate);
+        if (success_count > 0) {
+            printf("rtt min/avg/max = %.3f/%.3f/%.3f ms\n", min_time, avg_time, max_time);
+        }
+    }
+
 quit:
     tcping_freehostinfo(host);
     return retval;
